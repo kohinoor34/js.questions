4c002ffff65b2bfaaa2852dc058972cb42a614ba
kohinoor34 <kohinorkable712@gmail.com>
2025-05-04 22:33:23 +0530

    deep v shallow

--- index.js
+++ index.js
@@ -6,10 +6,34 @@
 // console.log(x);
 
 
-const arr = [1,2,3,4,5]
-for(var i = 0; i<arr.length;i++){
-    setTimeout(() => {
-        console.log(i);
+// const arr = [1,2,3,4,5]
+// for(var i = 0; i<arr.length;i++){
+//     setTimeout(() => {
+//         console.log(i);
         
-    }, 1000);
-}
+//     }, 1000);
+// }
+
+//shallow copy
+// let a = {
+//     x:1,
+//     y:2,
+// };
+// let b = a;
+// a.x = 5;
+// console.log(a);
+// console.log(b);  //this concept is shallow coppy and we can create deep copy of next
+
+
+//deep copy
+
+let a = {
+    x:1,
+    y:2,
+};
+let b = {...a}
+a.x = 5;
+console.log(a);
+console.log(b);
+
+
