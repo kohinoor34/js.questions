ba414a8054ce3083448d037f78e7b167e76f1e6a
kohinoor34 <kohinorkable712@gmail.com>
2025-05-04 22:24:03 +0530

    added another question

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let x = 1;
+// let x = 1;
 
-if(function f(){}){
-    x+= typeof f
+// if(function f(){}){
+//     x+= typeof f
+// }
+// console.log(x);
+
+
+const arr = [1,2,3,4,5]
+for(var i = 0; i<arr.length;i++){
+    setTimeout(() => {
+        console.log(i);
+        
+    }, 1000);
 }
-console.log(x);
