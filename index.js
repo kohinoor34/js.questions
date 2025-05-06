46e4d4d4bb6c9f6281d2fffca3d9dc7ebcd82138
kohinoor34 <kohinorkable712@gmail.com>
2025-05-06 21:00:58 +0530

    adding next example

--- index.js
+++ index.js
@@ -40,20 +40,57 @@
 
 //hoisting 
 
-function parent(){
-    var count = 0;
+//function parent(){
+//     var count = 0;
 
-    return function(){
-        count++
-        console.log(count);
+//     return function(){
+//         count++
+//         console.log(count);
         
+//     }
+// }
+//       let countvalue = parent()
+//      countvalue()
+//      countvalue()
+//      countvalue()
+//      countvalue()
+
+
+// var lenght = 6;
+// function fn() {
+//     console.log(this.lenght);
+// }
+
+// obj = {
+//     lenght:7,
+//     methode:function(fn){
+// arguments[0]()
+//     }
+// }
+// obj.methode(fn,1)
+
+
+function executeFirstArgument(callback) {
+    if (arguments.length > 0 && typeof arguments[0] === 'function') {
+      arguments[0](); // Execute the first argument (which should be a function)
+    } else {
+      console.error("No function provided as the first argument!");
     }
-}
-      let countvalue = parent()
-     countvalue()
-     countvalue()
-     countvalue()
-     countvalue()
+  }
+  
+  function sayHello() {
+    console.log("Hello!");
+  }
+  
+  // Correct usage:
+  executeFirstArgument(sayHello); // This will print "Hello!"
+  
+  // Incorrect usage:
+  executeFirstArgument(); // This will print "No function provided as the first argument!"
+  executeFirstArgument("not a function"); // This will print "No function provided as the first argument!"
+
+
+
       
       
       
