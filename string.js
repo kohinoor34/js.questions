35ef9c2f0fbddc2b3f7bbb16d4488392d7ef48cd
kohinoor34 <kohinorkable712@gmail.com>
2025-05-29 11:20:40 +0530

    remive blank space using predefined method

--- string.js
+++ string.js
@@ -1,5 +1,15 @@
-//convert anystring firstindex in uppercase
-const jvascript =  str => str.split(' ').map(word => word.charAt(0).toUpperCase()+ word.slice(1).toLowerCase())
+// 1. convert anystring firstindex in uppercase
+const jvascript =  str => str.split(' ').map(word => word.charAt(0).toUpperCase()+word.slice(1).toLowerCase())
 console.log(jvascript( 'javascript string tips'))
 
 // op = [ 'Javascript', 'String', 'Tips' ]
+
+
+//2.remove unwanted space
+
+const trimmed = "   hellow everyone  "
+console.log(trimmed);
+console.log(trimmed.trim());
+
+
+
